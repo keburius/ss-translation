eae5062d4d0f7598f102ec95e001c94c237b880d
Ubuntu <ubuntu@ip-172-31-14-238.ec2.internal>
2023-05-24 16:27:33 +0000

    loc

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS
 from model import translate_text
 import os
 
 os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'python'
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/translate', methods=['POST'])
+@app.route('/translate')
 def translate():
-    input_text = request.form['text']
-    output_language = request.form['target_lang']
+    input_text = request.args.get('text')
+    output_language = request.args.get('target_lang')
 
-    predicted_translation = translate_text(input_text, output_language)
+    if input_text and output_language:
+        predicted_translation = translate_text(input_text, output_language)
+        return jsonify({'translation': predicted_translation})
 
-    return jsonify({'translation': predicted_translation})
-
+    return jsonify({'error': 'Invalid request or missing data'})
 
 @app.route('/')
 def index():
     return "Welcome to Ss-Translator"
-
 
 if __name__ == '__main__':
     app.run()
